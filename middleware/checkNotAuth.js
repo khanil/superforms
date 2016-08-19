07404a2370b2340a121d567702552acc33c0fb25
Vyacheslav Khanilo <khaniloslava@yandex.ru>
2016-08-19 15:53:56 +0300

    fixed untracked files

--- middleware/checkNotAuth.js
+++
@@ -1,5 +0,0 @@
-var HttpError = require('../error').HttpError;
-
-module.exports = function(req, res, next) {
-	(!req.session.user) ? next() : next(new HttpError(401, "Вы уже авторизованы"));
-};
--- routes/main.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-
-exports.get = function (req, res, next) {
-	console.log('main starts: ', new Date())
-	res.render(req.session.user ? 'main' : 'signin');
-	console.log('main ends: ', new Date())
-};
